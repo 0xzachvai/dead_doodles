a9d8bad02344048724dcc55b24a4c52c564237de
0xzachvai <0xzachvai@gmail.com>
2022-01-16 00:02:54 -0800

    text change

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -49,7 +49,7 @@
 			<h2>Unleash the Undead</h2>
 
 			<h3 style="color: white">SUPPLY OF 4,000 UNDEAD DOODLES</h3>
-			<h3 style="color: white">20 MINTS PER WALLET, FIRST 1,000 ARE FREE</h3>
+			<h3 style="color: white">20 MINTS PER TRANSACTION, FIRST 1,000 ARE FREE</h3>
 			<h3 style="color: white">REST ARE SOLD AT A PRICE TBA</h3>
 		</div>
 	</Content>
