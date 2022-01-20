e705dacc4bd1fed4e7a11b0ff568d82c0b250b22
0xzachvai <0xzachvai@gmail.com>
2022-01-19 16:13:16 -0800

    missing image

--- index.js
+++ index.js
@@ -49,30 +49,30 @@ import { readFileSync, renameSync, writeFileSync } from 'fs';
 // }
 
 // UPDATE METADATA
-for (let i = 1; i < 4001; i++) {
-	let f = `./static/tokens/metadata/${i}.json`;
-	let data = JSON.parse(readFileSync(f));
+// for (let i = 1; i < 4001; i++) {
+// 	let f = `./static/tokens/metadata/${i}.json`;
+// 	let data = JSON.parse(readFileSync(f));
 
-	data.name = `Undead Doodle #${i}`;
-	data.image = `https://undeaddoodles.com/tokens/images/${i}.jpeg`;
+// 	data.name = `Undead Doodle #${i}`;
+// 	data.image = `https://undeaddoodles.com/tokens/images/${i}.jpeg`;
 
-	writeFileSync(f, JSON.stringify(data));
-}
+// 	writeFileSync(f, JSON.stringify(data));
+// }
 
 // MANUAL SWAPPING
-function swap(id1, id2) {
-	let path = './static/tokens';
-	renameSync(`${path}/images/${id1}.jpeg`, `tmp${id1}.jpeg`);
-	renameSync(`${path}/metadata/${id1}.json`, `tmp${id1}.json`);
+// function swap(id1, id2) {
+// 	let path = './static/tokens';
+// 	renameSync(`${path}/images/${id1}.jpeg`, `tmp${id1}.jpeg`);
+// 	renameSync(`${path}/metadata/${id1}.json`, `tmp${id1}.json`);
 
-	renameSync(`${path}/images/${id2}.jpeg`, `${path}/images/${id1}.jpeg`);
-	renameSync(`${path}/metadata/${id2}.json`, `${path}/metadata/${id1}.json`);
+// 	renameSync(`${path}/images/${id2}.jpeg`, `${path}/images/${id1}.jpeg`);
+// 	renameSync(`${path}/metadata/${id2}.json`, `${path}/metadata/${id1}.json`);
 
-	renameSync(`tmp${id1}.jpeg`, `${path}/images/${id1}.jpeg`);
-	renameSync(`tmp${id1}.json`, `${path}/metadata/${id2}.json`);
-}
+// 	renameSync(`tmp${id1}.jpeg`, `${path}/images/${id1}.jpeg`);
+// 	renameSync(`tmp${id1}.json`, `${path}/metadata/${id2}.json`);
+// }
 
-swap(1500, 2000);
+// swap(1500, 2000);
 
 for (let i = 1; i < 4001; i++) {
 	let f = `./static/tokens/metadata/${i}.json`;
